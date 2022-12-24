9477da89b0c01f1c5f44e0453e4fa58bffcf5882
Harsh <harshgupta3341@gmail.com>
2022-12-24 21:32:48 +0530

    done with config file

---
+++ config/config.go
@@ -0,0 +1,45 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+
+var (
+	Token     string
+	BotPrefix string
+
+	config *configStruct
+)
+
+type configStruct struct {
+	Token     string `json:"token"`
+	BotPrefix string `json:"botPrefix"`
+}
+
+func ReadConfig() error {
+	fmt.Println("Reading config file...")
+
+	file, err := ioutil.ReadFile("./config.json")
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+
+	fmt.Println(string(file))
+
+	err = json.Unmarshal(file, &config)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+
+	Token = config.Token
+	BotPrefix = config.BotPrefix
+
+	return nil
+
+}
