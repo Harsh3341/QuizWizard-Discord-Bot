ab7520da138af14beae913963f18fd93d0c8b218
Harsh <harshgupta3341@gmail.com>
2022-12-24 21:33:45 +0530

    added function to start bot

---
+++ bot/bot.go
@@ -0,0 +1,49 @@
+package bot
+
+import (
+	"fmt"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/harsh3341/3rd-Semester-Mini-Project/config"
+)
+
+var BotID string
+var goBot *discordgo.Session
+
+func Start() {
+	goBot, err := discordgo.New("Bot " + config.Token)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	u, err := goBot.User("@me")
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	BotID = u.ID
+
+	goBot.AddHandler(messageHandler)
+
+	err = goBot.Open()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Println("Bot is running...")
+}
+
+func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == BotID {
+		return
+	}
+
+	if m.Content == config.BotPrefix+"ping" {
+		_, _ = s.ChannelMessageSend(m.ChannelID, "pong")
+	}
+}
